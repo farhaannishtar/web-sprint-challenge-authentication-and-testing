fb983f70c78a878525ad55f555ec9c4bfda9d982
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2022-05-30 11:44:04 -0400

    Restricted handling 2

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -5,21 +5,25 @@ const { JWT_SECRET } = require('../../config');
 module.exports = async (req, res, next) => {
   console.log("Move on! Don't trip over a girl. There is an ocean of them out there. Abundance Mindset")
   
-  if (req.headers.authorization === null) {
-    res.status(401).json({message: 'token required' });
+  if (req.headers.authorization == null) {
+    // next({ status: 401, message: 'token required' });
+    res.status(401).json({
+      message: 'token required'
+    })
     return;
   }
 
-  try {
-    req.decodedJwt = await jwt.verify(req.headers.authorization, JWT_SECRET);
-    let user = await Users.findById(req.decodedJwt.sub);
-    if(req.decodedJwt.iat < user.logout_time) {
-      res.status(401).json({message: 'token invalid' });
-      return;
-    }
-  } catch(err) {
-    return;
-  }
+  // try {
+  //   req.decodedJwt = await jwt.verify(req.headers.authorization, JWT_SECRET);
+  //   let user = await Users.findById(req.decodedJwt.sub);
+  //   if(req.decodedJwt.iat < user.logout_time) {
+  //     next({ status: 401, message: 'token invalid' });
+  //     return;
+  //   }
+  // } catch(err) {
+  //   // next({ status: 401, message: 'this endpoint is restricted' });
+  //   return;
+  // }
 
   next();
   /*
